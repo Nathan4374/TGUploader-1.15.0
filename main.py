909ab479e680a2032379566532df0b8f91340b41
Nathan4374 <102322972+Nathan4374@users.noreply.github.com>
2022-07-01 14:27:55 -0500

    Update main.py

--- main.py
+++ main.py
@@ -454,20 +454,7 @@ def onmessage(update,bot:ObigramClient):
                     tuto.close()
                 return
 
-        if '/dir' in msgText:
-            try:
-                cmd = str(msgText).split(' ',2)
-                repoid = cmd[1]
-                getUser = user_info
-                if getUser:
-                    getUser['dir'] = repoid + '/'
-                    jdb.save_data_user(username,getUser)
-                    jdb.save()
-                    statInfo = infos.createStat(username,getUser,jdb.is_admin(username))
-                    bot.sendMessage(update.message.chat.id,statInfo)
-            except:
-                bot.sendMessage(update.message.chat.id,'❌Error en el comando /dir folder❌')
-            return
+        
         if '/cancel_' in msgText:
             try:
                 cmd = str(msgText).split('_',2)
